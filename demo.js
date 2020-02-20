cf2549ab0c6111df02672c96ba241b6759851ca9
Rob Martin <rob.martin.5758@gmail.com>
2020-02-20 10:33:31 -0500

    bunch of changes

--- demo.js
+++ demo.js
@@ -100,40 +100,100 @@
   display(kid.isAdult());
 
 
-// use value of var to find object property - need to use braket notation
-for (let propertyName in freddy) {
-  display(propertyName + ': ' + freddy[propertyName] )
-}
-
-// object properties
-
-display(Object.getOwnPropertyDescriptor(freddy, 'firstName'))
-Object.defineProperty(freddy, 'firstName', { writable: false})
-display(Object.getOwnPropertyDescriptor(freddy, 'firstName'))
-
-Object.defineProperty(freddy, 'firstName', { writable: true})
-
-
-// get set
-display(Object.getOwnPropertyDescriptor(freddy, 'firstName'));
-
-Object.defineProperty(freddy, 'fullName', { 
-  get: function() {
-    return this.firstName + ' ' + this.lastName
-  },
-  set: function(value) {
-    var nameParts = value.split(' ');
-    display('nameParts: ' + JSON.stringify(nameParts))
-    this.firstName = nameParts[0];
-    this.lastName = nameParts[1];
+  // use value of var to find object property - need to use braket notation
+  for (let propertyName in freddy) {
+    display(propertyName + ': ' + freddy[propertyName])
   }
-});
 
-display('Full Name: ' + freddy.fullName);
-display('freddy: ' + JSON.stringify(freddy))
-freddy.fullName = 'Willie Mays';
-display('New Full Name: ' + freddy.fullName);
+  // object properties
+
+  display(Object.getOwnPropertyDescriptor(freddy, 'firstName'))
+  Object.defineProperty(freddy, 'firstName', { writable: false })
+  display(Object.getOwnPropertyDescriptor(freddy, 'firstName'))
+
+  Object.defineProperty(freddy, 'firstName', { writable: true })
 
 
+  // get set
+  display(Object.getOwnPropertyDescriptor(freddy, 'firstName'));
+
+  Object.defineProperty(freddy, 'fullName', {
+    get: function () {
+      return this.firstName + ' ' + this.lastName
+    },
+    set: function (value) {
+      var nameParts = value.split(' ');
+      display('nameParts: ' + JSON.stringify(nameParts))
+      this.firstName = nameParts[0];
+      this.lastName = nameParts[1];
+    }
+  });
+
+  display('Full Name: ' + freddy.fullName);
+  display('freddy: ' + JSON.stringify(freddy))
+  freddy.fullName = 'Willie Mays';
+  display('New Full Name: ' + freddy.fullName);
+  // *************************************
+  // PROTOTYPES
+  display('PROTOTYPES')
+
+  // *************************************
+
+  let testFunc1 = function () { };
+
+  display(testFunc1.prototype) // empty object named testFunc1
+
+  let testObj = { name: 'Willie' }
+  display(testObj.prototype) // undefined
+  display(testObj.__proto__) // empty object, no name, just Object
+
+  // FUNCTION prototype is the object instance that will become the prototype 
+  // for all objects created using this function as a constructor
+
+  // OBJECT prototype is the object instance from which the object is inherited
+
+  function Person2(fName, lName) {
+    this.fName = fName;
+    this.lName = lName;
+    this.height = 72;
+  }
+  display('Person2: ' + Person2) // function name and details - looks just like definition
+  display('Person2 prototype Before age added ' + JSON.stringify(Person2.prototype)); // empty
+  Person2.prototype.age = 25;
+  display('After age added prototype: ' + JSON.stringify(Person2.prototype)); // contains age, empty id age not added
+  display('After age added __proto__.age: ' + Person2.__proto__.age); // undefined
+  display('After age added __proto__: ' + Person2.__proto__); // function() {[native code]}
+
+  // __proto__ is shared by all - any change affects all - change willie age also change roberto and nolan
+  // prototype exists for the parent, children have __proto__ that points to same instance as parent prototype
+  let willie = new Person2('Willie', 'Mays');
+  display('Willie  prototype: ' + JSON.stringify(willie.prototype)); // undefined
+  display('Willie  __proto__: ' + willie.__proto__.age); //  25
+
+  // this changes age for everyone because __proto__ points (references) Person2 prototype
+  willie.__proto__.age = 30;
+  // willie.prototype.age = 45; // not valid 
+
+  let roberto = new Person2('Roberto', 'Clemente');
+  display('Roberto  prototype: ' + JSON.stringify(roberto.prototype)); // undefined
+  display('Roberto  __proto__: ' + roberto.__proto__.age); //  30
+
+  let nolan = new Person2('Nolan', 'Ryan');
+  display('Nolan  prototype: ' + JSON.stringify(nolan.prototype)); // undefined
+  display('Nolan  __proto__: ' + nolan.__proto__.age); //  30
+
+  display('prototype and __proto__ are same for function and an instance: ')
+  display(Person2.prototype === nolan.__proto__) // true
+
+  roberto.age = 35;
+  display('Roberto  prototype after roberto.age change : ' + JSON.stringify(roberto.prototype)); // undefined
+  display('Roberto  __proto__ after roberto.age change: ' + roberto.__proto__.age); //  30
+  display('Roberto  age after roberto.age change: ' + roberto.age); //  35
+  display('Roberto  after roberto.age change: ' + JSON.stringify(roberto)); //  fname, lname, height, age = 35
+  display('Willie  after roberto.age change: ' + JSON.stringify(willie)); //  fname, lname, height, NO AGE
+  // only roberto age changed
+  display('Nolan  prototype after roberto.age change : ' + JSON.stringify(nolan.prototype)); // undefined
+  display('Nolan  __proto__ after roberto.age change: ' + nolan.__proto__.age); //  30
+  display('Nolan  age after roberto.age change: ' + nolan.age); //  30
 
 })();
